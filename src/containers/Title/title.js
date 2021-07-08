4043fe7e64eb2f6891c2ee616d8c5a3312ce999d
Sofia Alvarado <alvaradosofiah@outlook.com>
2021-07-07 18:17:12 -0600

    adding title

---
+++ src/containers/Title/title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+
+const Title = () => (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Bitcoin Price</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+     
+      </Navbar.Collapse>
+    </Navbar>
+    
+    )
+    
+    export default Title;
